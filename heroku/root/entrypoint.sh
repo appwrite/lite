08e84970b02baf7f2867e5ec5824c72cc3e2e3dd
Damodar Lohani <dlohani48@gmail.com>
2021-02-21 17:43:08 +0545

    support redis addon

--- heroku/root/entrypoint.sh
+++ heroku/root/entrypoint.sh
@@ -16,6 +16,22 @@ export _APP_DB_SCHEMA=$schema
 export _APP_DB_USER=$user
 export _APP_DB_PASS=$pass
 
+# Redis
+redis=$REDIS_URL
+proto="$(echo $redis | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+url="$(echo ${redis/$proto/})"
+userpass="$(echo $url | grep @ | cut -d@ -f1)"
+user="$(echo $userpass | grep : | cut -d: -f1)"
+pass="$(echo $userpass | grep : | cut -d: -f2)"
+hostport="$(echo ${url/$userpass@/} | cut -d/ -f1)"
+host="$(echo $hostport | sed -e 's,:.*,,g')"
+port="$(echo $hostport | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
+
+export _APP_REDIS_HOST=$host
+export _APP_REDIS_PORT=$port
+export _APP_REDIS_USER=$user
+export _APP_REDIS_PASS=$pass
+
 php /dbInit.php
 
 /usr/bin/supervisord -n -c /etc/supervisord.conf
